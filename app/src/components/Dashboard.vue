d3da0c5cc8d388eac992da657b75ad7acf04359e
Malo Hamon <hamon.malo@gmail.com>
2022-08-28 12:44:59 +1000

    Team creating plugin

--- app/src/components/Dashboard.vue
+++ app/src/components/Dashboard.vue
@@ -1,10 +1,109 @@
 <template>
-  <div>Example Plugin Dashboard Control</div>
+  <div class="q-pa-md" style="max-width: 350px">
+    <p>
+      When pressing 'assign to teams', a team will be created for each breakout
+      room with each attendee assigned as a member
+    </p>
+
+    <div v-if="loading" class="loading">Loading ...</div>
+    <div v-if="!loading">
+      <div>
+        <q-btn @click="createTeams" label="Assign to teams" />
+      </div>
+      <div v-if="teams !== null" class="sucess">Sucess</div>
+      <div v-if="error !== null">
+        <div class="error">error</div>
+        <div>{{ this.error }}</div>
+      </div>
+
+      <div v-if="teams !== null" v-for="team in teams" class="team">
+        <div class="teamName">{{ team.teamName }}</div>
+        <q-list bordered separator>
+          <q-item
+            clickable
+            v-ripple
+            :active="active"
+            v-for="member in team.members"
+          >
+            <q-item-section>{{ member.username }}</q-item-section>
+            <q-item-section side>{{ member.userRole }}</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple :active="active">
+            <q-item-section>{{ team.sensorId }}</q-item-section>
+            <q-item-section side>ZoomSense Bot</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { saveBreakoutRoomsAsTeams } from "./createTeams";
 export default {
   name: "Dashboard",
-  props: {},
+  data() {
+    return {
+      teams: null,
+      error: null,
+      loading: false,
+    };
+  },
+  props: {
+    context: { required: true },
+    firebase: { required: true },
+    functions: { required: true },
+  },
+  watch: {
+    firebase: {
+      immediate: true,
+      async handler() {
+        if (!this.context.meetingid) return;
+      },
+    },
+  },
+  methods: {
+    async createTeams() {
+      this.loading = true;
+      saveBreakoutRoomsAsTeams(this.context.meetingid, this.firebase)
+        .then((result) => {
+          this.error = null;
+          this.teams = result;
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.loading = false;
+
+          this.error = error;
+        });
+    },
+  },
 };
 </script>
+
+<style>
+.team {
+  margin: 30px;
+}
+.teamName {
+  font-size: 20px;
+  font-weight: bold;
+}
+.sucess {
+  font-size: 20px;
+  font-weight: bold;
+  color: green;
+  margin-top: 20px;
+}
+.error {
+  font-size: 20px;
+  font-weight: bold;
+  color: red;
+  margin-top: 20px;
+}
+.loading {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+</style>
